c490a517e57d1e2c375261ea52f6762a910188ee
lemonliu1993 <2305247091@qq.com>
2024-11-19 08:53:09 +0800

    JVM锁失效之一——多例模式

--- src/main/java/com/lemon/distributed/lock/service/StockService.java
+++ src/main/java/com/lemon/distributed/lock/service/StockService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.lemon.distributed.lock.mapper.StockMapper;
 import com.lemon.distributed.lock.pojo.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.locks.ReentrantLock;
@@ -12,33 +14,22 @@ import java.util.concurrent.locks.ReentrantLock;
  * Created by lemoon on 2024/7/7 11:10
  */
 @Service
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class StockService {
 
-//    private Stock stock = new Stock();
 
     private ReentrantLock lock = new ReentrantLock();
 
     @Autowired
     private StockMapper stockMapper;
 
-//    public synchronized void deduct() {
-//        stock.setStock(stock.getStock() - 1);
-//        System.out.println("库存余量: " + stock.getStock());
-//    }
 
-
-    public void deduct() {
-//        lock.lock();
-//        try {
-            Stock stock = stockMapper.selectOne(new QueryWrapper<Stock>().eq("product_code", "1001"));
-            if (stock != null && stock.getCount() > 0) {
-                stock.setCount(stock.getCount() - 1);
-                this.stockMapper.updateById(stock);
-//            }
-//        } finally {
-//            lock.unlock();
+    public synchronized void deduct() {
+        Stock stock = stockMapper.selectOne(new QueryWrapper<Stock>().eq("product_code", "1001"));
+        if (stock != null && stock.getCount() > 0) {
+            stock.setCount(stock.getCount() - 1);
+            this.stockMapper.updateById(stock);
         }
     }
-
 
 }
